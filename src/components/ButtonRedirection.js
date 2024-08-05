758066658c1ec6fe9c04605919cbbf06624b4bba
Mohamed Marzougui <m.marzougui@it-students.fr>
2024-08-05 10:56:56 +0200

    change menuText color to black

--- src/components/ButtonRedirection.js
+++ src/components/ButtonRedirection.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
 export default function ButtonRedirection({ text, url, navigation }) {
   return (
-    <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate(url)}>
+    <TouchableOpacity
+      style={styles.menuItem}
+      onPress={() => navigation.navigate(url)}
+    >
       <Text style={styles.menuText}>{text}</Text>
     </TouchableOpacity>
   );
@@ -13,10 +16,10 @@ const styles = StyleSheet.create({
   menuItem: {
     padding: 16,
     borderBottomWidth: 1,
-    borderBottomColor: '#333',
+    borderBottomColor: "#333",
   },
   menuText: {
-    color: 'white',
+    color: "black",
     fontSize: 18,
   },
 });
